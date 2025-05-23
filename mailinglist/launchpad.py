de581512109c61fc69d72104225b14b5b069e4af
Christian Krinitsin <mail@krinitsin.com>
2025-05-23 13:18:00 +0200

    fix return values

--- mailinglist/launchpad.py
+++ mailinglist/launchpad.py
@@ -3,14 +3,14 @@ from os import makedirs, path
 
 def process_launchpad_bug(bug_id) -> bool:
     if path.exists(f"output_launchpad/{bug_id}"):
-        return false
+        return False
 
     bug_url = f"https://api.launchpad.net/1.0/bugs/{bug_id}"
 
     bug_response = get(url = bug_url)
 
     if not 'application/json' in bug_response.headers.get('Content-Type', ''):
-        return false
+        return False
 
     bug_data = bug_response.json()
 
@@ -24,4 +24,4 @@ def process_launchpad_bug(bug_id) -> bool:
 
         for entry in messages_data['entries']:
             file.write(f"{entry['content']}\n\n")
-    return true
+    return True
