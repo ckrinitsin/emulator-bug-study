dbbaa64f16cef5a2b32056a67433116dab84ab81
Christian Krinitsin <mail@krinitsin.com>
2025-05-29 17:10:08 +0200

    first version of categories in the classifier

--- classification/main.py
+++ classification/main.py
@@ -1,10 +1,17 @@
 from transformers import pipeline
+from os import listdir, path
 
+directory : str = "./test_mails"
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-with open("test", "r") as file:
-    sequence_to_classify = file.read()
-candidate_labels = ['semantic bug', 'no semantic bug']
-result = classifier(sequence_to_classify, candidate_labels, multi_label=False)
 
-print(result['labels'])
-print(result['scores'])
+for name in listdir(directory):
+    with open(path.join(directory, name), "r") as file:
+        sequence_to_classify = file.read()
+
+    candidate_labels = ['semantic', 'other', 'mistranslation', 'instruction']
+    result = classifier(sequence_to_classify, candidate_labels, multi_label=True)
+
+    print(name)
+    for label, score in zip(result["labels"], result["scores"]):
+        print(f"{label}: {score:.3f}")
+    print("\n")
