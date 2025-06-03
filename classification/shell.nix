e5317707f115fc8d6423e1d850c6b52d1f9edbb0
Christian Krinitsin <mail@krinitsin.com>
2025-06-03 12:02:16 +0000

    fix shell.nix

--- classification/shell.nix
+++ classification/shell.nix
@@ -2,12 +2,13 @@
 with pkgs;
 mkShell {
   buildInputs = [
-    python312Packages.numpy
     python312Packages.transformers
     python312Packages.torch
   ];
   shellHook = ''
     # fixes libstdc++ issues and libgl.so issues
     LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
+    # fixes unallocation issues
+    export MKL_NUM_THREADS=64
   '';
 }
