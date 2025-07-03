5541099586dbd6018574cb44e1934907c121526f
Christian Krinitsin <mail@krinitsin.com>
2025-07-03 18:24:16 +0200

    edit tool and add result analysis

--- classification/tools/create_csv.py
+++ classification/tools/create_csv.py
@@ -1,6 +1,12 @@
 from os import listdir, path
+from argparse import ArgumentParser
 
 root_directory = "../../results/classifier/"
+
+parser = ArgumentParser()
+parser.add_argument('-d', '--directory')
+
+args = parser.parse_args()
 
 def parse_iteration(directory):
     dictionary = {}
@@ -19,6 +25,11 @@ def output_csv(dictionary, full_path):
             file.write(f"{key}, {value}\n")
 
 def main():
+    if args.directory:
+        dictionary = parse_iteration(args.directory)
+        output_csv(dictionary, args.directory)
+        exit()
+
     for entry in listdir(root_directory):
         full_path = path.join(root_directory, entry)
         if path.isdir(full_path):
